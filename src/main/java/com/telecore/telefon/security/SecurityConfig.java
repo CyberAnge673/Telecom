b6f31d74dea455720d147e33dde6b5e512302c03
HC-ONLINE <henriquezandres856@gmail.com>
2026-02-01 14:00:11 -0500

    Agregar filtro JwtRequestFilter para manejo de autenticación JWT en las solicitudes

--- src/main/java/com/telecore/telefon/security/SecurityConfig.java
+++ src/main/java/com/telecore/telefon/security/SecurityConfig.java
@@ -1,23 +1,26 @@
 package com.telecore.telefon.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,35 +28,35 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sin estado
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
                         // Permitir crear usuarios y extensiones sin estar logueado
-                        .requestMatchers(HttpMethod.POST, "/Usuario", "/extensiones").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/Usuario", "/extensiones").permitAll() // TODO cambiar a
+                                                                                                  // autenticado
                         // Permitir consultar usuarios y extensiones
                         .requestMatchers(HttpMethod.GET, "/Usuario/**", "/extensiones/**").permitAll()
                         // Cualquier otra ruta requiere autenticación
-                        .anyRequest().authenticated()
-                )
+                        .anyRequest().authenticated())
                 .formLogin(form -> form.disable()) // Desactivar formulario
                 .httpBasic(Customizer.withDefaults()) // Permitir Auth básica para pruebas
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
-        return  authenticationConfiguration.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
 
     }
 
-
-//    @Bean
-//    public UserDetailsService userDetailsService(){
-//
-//    }
-
-
+    // TODO eliminar si no se usara
+    // @Bean
+    // public UserDetailsService userDetailsService(){
+    //
+    // }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
-
